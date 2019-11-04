2d4d096d7ddbba475bbb3e5ecf6d0b0a3640d24f
JinYuSha0 <1009943858@qq.com>
2019-11-04 18:09:38 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# thing
-# thing
+# asyncCompose
--- index.js
+++ index.js
@@ -1,142 +1,52 @@
-// 标记成功状态
-const successSymbol = Symbol('success')
-
-const compose = (...fns) => x => fns.reduce((p, fn) => {
-    return p.then((...args) => {
-        if (args[0][successSymbol] !== false) {
-            return fn.execute(...args)
+function asyncCompose (...fns){
+    return function (...args) {
+        return fns.reduce((a, b) => {
+            if (isPromise(a)) {
+            return a.then((...p) => b(...p))
+        } else if (isAsyncFunction(a)) {
+            return a(...args).then((...p) => b(...p))
+        } else if (isFunction(a)) {
+            return b(a(...args))
+        } else {
+            return b(a)
         }
     })
-}, Promise.resolve(x))
-const compose2 = (...fns) => x => {
-    fns = fns.reverse()
-    return fns.reduce((p, fn) => p.then(fn), Promise.resolve(x))
-}
-//组合函数
-/*function compose(...funcs) {
-    return funcs.reduce((a, b) => (...args) => {
-        const nextArgs = b.execute.call(b, ...args)
-        if (nextArgs[successSymbol]) {
-            return a.execute.call(a, { ...nextArgs })
-        }
-    })
-}*/
-
-function logger (content) {
-    console.log(content)
-}
-
-const foods = {
-    beef: {
-        num: 100,
-        price: 15
     }
 }
-
-const users = {
-    'sjy': {
-        money: 100
-    }
+function isPromise(o) {
+    if (o == null) return false
+    return Object.getPrototypeOf(o) === Promise.prototype
+}
+function isFunction(o) {
+    if (o == null) return false
+    return Object.getPrototypeOf(o) === Function.prototype
+}
+function isAsyncFunction(o) {
+    if (o == null) return false
+    return o.constructor.name === 'AsyncFunction'
 }
 
-const api = {
-    pay: ({ foodType, num, user }) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (Math.random() > 0.1) {
-                    users[user].money -= foods[foodType].price * num
-                    resolve()
-                } else {
-                    reject()
-                }
-            }, 1000)
-        })
-    },
-    refund: ({ foodType, num, user }) => {
-        users[user].money += foods[foodType].price * num
-    },
-    insertDB: ({ foodType, num, user }) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (Math.random() > 0.9) {
-                    window.localStorage.setItem(`${user}-${foodType}-${num}`, new Date().getTime())
-                    resolve()
-                } else {
-                    reject()
-                }
-            }, 1000)
-        })
-    }
+/* DEMO */
+function delay(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => { resolve() }, ms)
+})
 }
 
-// 流程一: 减少食物总量
-const step1 = {
-    execute: async function ({ foodType, num, user, rollbackList = [] }) {
-        if (foods[foodType] && foods[foodType].num > 0) {
-            foods[foodType].num -= num
-            rollbackList.push(this.rollback)
-            logger(`流程一: 减少食物总量执行成功,目前${foodType}总量为${foods[foodType].num}`)
-            return { ...arguments[0], rollbackList, [successSymbol]: true }
-        }
-    },
-    rollback: async function ({ foodType, num, user }) {
-        if (foods[foodType]) {
-            foods[foodType].num += num
-            logger(`流程一: 减少食物总量回滚成功,目前${foodType}总量为${foods[foodType].num}`)
-            return { ...arguments[0], [successSymbol]: true }
-        }
-    }
+asyncCompose(
+    (ms) => { console('start'); return ms * 2 },
+    async (ms) => { await delay(ms); return ms},
+    (ms) => console(`delay ${ms/1000}s`),
+    async () => await delay(1000),
+    () => console('Hello'),
+    async () => await delay(500),
+    async () => await delay(600),
+    async () => await delay(700),
+    () => console('world'),
+)(1000)
+
+function console(c) {
+    let p = document.createElement('p')
+    p.innerText = c
+    document.body.appendChild(p)
 }
-
-// 流程二: 在线支付
-const step2 = {
-    execute: async function ({ foodType, num, user, rollbackList = [] }) {
-        try {
-            await api.pay({ foodType, num, user })
-            rollbackList.push(this.rollback)
-            logger(`流程二: 在线支付执行成功,目前${user}余额为${users[user].money}`)
-            return { ...arguments[0], rollbackList, [successSymbol]: true }
-        } catch (e) {
-            const { rollbackList, ...args } = arguments[0]
-            logger(`流程二: 在线支付执行失败,目前${user}余额为${users[user].money}`)
-            compose2(...rollbackList)({ ...args })
-            return { ...arguments[0], rollbackList, [successSymbol]: false }
-        }
-    },
-    rollback: async function ({ foodType, num, user }) {
-        api.refund({ foodType, num, user, [successSymbol]: true })
-        logger(`流程二: 在线支付回滚成功,目前${user}余额为${users[user].money}`)
-        return { ...arguments[0], [successSymbol]: true }
-    }
-}
-
-// 流程三: 插入数据库
-const step3 = {
-    execute: async function ({ foodType, num, user, rollbackList = [] }) {
-        try {
-            await api.insertDB({ foodType, num, user })
-            rollbackList.push(this.rollback)
-            logger(`流程三: 插入数据库执行成功`)
-            return { ...arguments[0], rollbackList, [successSymbol]: true }
-        } catch (e) {
-            const { rollbackList, ...args } = arguments[0]
-            logger(`流程三: 插入数据库执行失败`)
-            compose2(...rollbackList)({ ...args })
-            return { ...arguments[0], rollbackList, [successSymbol]: false }
-        }
-    },
-    rollback: async function () {
-        // 不做了
-        logger(`流程三: 插入数据库回滚成功`)
-        return { ...arguments[0], [successSymbol]: true }
-    }
-}
-
-const order = compose(
-    step1,
-    step2,
-    step3
-)
-
-order({ foodType: 'beef', num: 2, user: 'sjy' })
-
